32e1f9de5684e10df0db5a6b6239da437145a87d
Charlie Ticer <chticer@outlook.com>
2026-07-07 20:44:04 -0500

    Add project directory
    This adds the project directory code infrastructure for setting the
    root and shared directory depending on the user environment. This also
    adds a helper method for initializing a module from the backend.

---
+++ backend/utils/project-directory.js
@@ -0,0 +1,20 @@
+import path from "path";
+import dotenv from "dotenv";
+import { pathToFileURL } from "url";
+
+dotenv.config();
+
+const rootDirectoryBase = process.env.ROOT_DIRECTORY || "./";
+
+const ROOT_DIRECTORY = path.resolve(process.cwd(), rootDirectoryBase);
+
+const PROJECT_DIRECTORY =
+{
+    "ROOT": ROOT_DIRECTORY,
+    "SHARED": path.join(ROOT_DIRECTORY, "shared")
+};
+
+export const getSharedModulePath = (fileName) =>
+{
+    return pathToFileURL(path.join(PROJECT_DIRECTORY["SHARED"], fileName)).href;
+};
