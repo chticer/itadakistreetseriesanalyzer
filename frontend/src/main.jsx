048ab339f725c0447db4a3f1991fadea408f322e
Charlie Ticer <chticer@outlook.com>
2026-07-09 21:35:33 -0500

    Update root rendering
    This updates the rendering of the main root HTML element to replace the
    default React component with a browser router containing the home page
    component.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { GlobalStateProvider } from "./contexts/GlobalStateContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "./index.css";
-import App from "./App";
+import "./global.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter
+(
+    [
+        {
+            path: "/",
+            element: <Layout />,
+            children:
+            [
+                {
+                    element: <Home />,
+                    index: true
+                }
+            ]
+        }
+    ]
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render
 (
     <React.StrictMode>
         <GlobalStateProvider>
-            <App />
+            <RouterProvider router={router} />
         </GlobalStateProvider>
     </React.StrictMode>
 );
