69b7bc4bf3fc39c49508acef0c7ebfe701f2f1ef
Charlie Ticer <chticer@outlook.com>
2026-07-09 21:28:32 -0500

    Update Vite configuration
    This updates the configuration of the Vite project to use environment
    variables. This also updates the use of a proxy for the backend server.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
 
+let BACKEND_PORT = 11004;
+
+try
+{
+    BACKEND_PORT = import.meta.env.VITE_BACKEND_PORT;
+}
+catch
+{
+}
+
+let PORT = 10004;
+
+try
+{
+    PORT = import.meta.env.VITE_PORT;
+}
+catch
+{
+}
+
 // https://vite.dev/config/
 export default defineConfig
 (
@@ -13,7 +33,15 @@ export default defineConfig
         ],
         server:
         {
-            port: 10004
+            port: PORT,
+            proxy:
+            {
+                "/api":
+                {
+                    target: `http://localhost:${BACKEND_PORT}`,
+                    changeOrigin: true
+                }
+            }
         }
     }
 );
