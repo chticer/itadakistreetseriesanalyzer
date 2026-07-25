dab37b22cf56c6551c78ce093febe6e5fbf87563
Charlie Ticer <chticer@outlook.com>
2026-07-25 10:40:04 -0500

    Add home page
    This adds a home page to the routes with scaffolding.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,20 @@
+import { getFrontendRoutePathsMappingCache } from "../utils/route-paths-cache.js";
+import Title from "../components/Title";
+
 const Home = () =>
 {
+    const FRONTEND_ROUTE_PATHS_MAPPING = getFrontendRoutePathsMappingCache();
+
+    const PATH = FRONTEND_ROUTE_PATHS_MAPPING.INDEX.INDEX.FULL_PATH;
+
+    const TitleComponent = () => <Title path={PATH} />;
+
     return (
     <>
+        <TitleComponent />
+
+        <main>
+        </main>
     </>
     );
 };
